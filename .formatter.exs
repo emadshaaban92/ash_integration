dc30615e091ab48a46bb0bd9ec850d4191fe8be1
Emad Shaaban <emad.shaaban92@gmail.com>
2026-03-29 15:30:39 +0200

    Export formatter locals_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,12 @@
+spark_locals_without_parens = [
+  resource_identifier: 1,
+  loader: 1,
+  supported_versions: 1,
+  outbound_action: 1,
+  ash_integration_dashboard: 1,
+  ash_integration_dashboard: 2
+]
+
 [
   import_deps: [
     :ash,
@@ -10,12 +19,8 @@
   ],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
   plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
-  locals_without_parens: [
-    resource_identifier: 1,
-    loader: 1,
-    supported_versions: 1,
-    outbound_action: 1,
-    ash_integration_dashboard: 1,
-    ash_integration_dashboard: 2
+  locals_without_parens: spark_locals_without_parens,
+  export: [
+    locals_without_parens: spark_locals_without_parens
   ]
 ]
